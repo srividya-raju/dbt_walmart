055e029f1f65e54a08cf92983701467bc2190d69
srividya raju <srividya.dec09@gmail.com>
2026-08-29 12:37:47 +0000

    create date logic

--- models/gold/Walmart_date_dim.sql
+++ models/gold/Walmart_date_dim.sql
@@ -1,6 +1,32 @@
+{{
+    config
+    (
+        materialized='incremental',
+        incremental_strategy='update+insert',
+        unique_key='Date_Id'
+
+    )
+
+}}
+
+with date_dim as (
 select ABS(HASH(f.Date)) AS Date_Id ,
 f.date,
 f.Isholiday,
-current_timestamp() as Insert_date,
+{% if is_incremental() %}
+
+    Insert_date,
+{% else %}
+    current_timestamp() as Insert_date,
+{% endif %}
+
 current_timestamp() as Update_date
 from {{ ref('int_date') }} f
+
+{% if is_incremental() %}
+where Insert_date > (select max(Update_date) from {{this}})
+{% endif%}
+
+)
+
+select * from date_dim
