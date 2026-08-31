26d80c745b28b1d088133151206f798371eb23b1
srividya raju <srividya.dec09@gmail.com>
2026-08-31 16:08:46 +0000

    update

--- models/gold/Walmart_date_dim.sql
+++ models/gold/Walmart_date_dim.sql
@@ -10,12 +10,17 @@
 }}
 
 with date_dim as (
-select ABS(HASH(f.Date)) AS Date_Id ,
+f.Date_Id ,
 f.date,
 f.Isholiday,
-f.Insert_date,
-CURRENT_TIMESTAMP() AS Update_date
+{% if is_incremental() %}
 
+    Insert_date,
+{% else %}
+    current_timestamp() as Insert_date,
+{% endif %}
+
+current_timestamp() as Update_date
 from {{ ref('int_date') }} f
 
 {% if is_incremental() %}
